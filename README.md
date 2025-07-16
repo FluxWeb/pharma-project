4ce795378cb89a135dfcfd1a7d8a0265320b10a5
Florent Weber <weberfernand.florent@yahoo.fr>
2025-07-16 12:51:01 +0200

    save

--- README.md
+++ README.md
@@ -1,9 +1,18 @@
 # molecularb generation project
 
 
-### creation d environnement 
-python -m venv mol_gen
+necessite python 3.12 ou moins pour installer torch avec CUDA
 
+j utilise la version 3.11
+
+compiler avec la version python 
+py -3.11 mon_script.py
+
+mettre à jour pip
+
+### creation d environnement 
+
+py -3.11 -m venv mol_gen
 
 ### activer environnement
 
@@ -31,7 +40,22 @@ deactivate
 ### supprimer environnement
 rm -r mol_gen
 
+windows: rmdir /S /Q mol_gen
+
+
+pour recuperer les donnees directement dans pip dans le requirements.txt il faut run la commande
+    pip freeze > requirements.txt
 
 ## datasets
 
 MOSES : https://github.com/molecularsets/moses
+
+
+# TO DO::
+
+
+Mettre lien vers article
+mieux rediger read me
+implementer plusieurs modele generatif
+faire benchmark performance
+interface
---
+++ python/data_manipulation.py
@@ -0,0 +1,92 @@
+import pandas as pd
+import numpy as np
+
+
+moses_data_path = "./data/moses/dataset_v1.csv"
+moses_data_train_path = "./data/moses/train.csv"
+moses_data_test_path = "./data/moses/test.csv"
+
+
+def load_data(file_path):    
+    """
+    Load data from a CSV file into a pandas DataFrame.
+    
+    Parameters:
+    file_path (str): The path to the CSV file.
+    
+    Returns:
+    pd.DataFrame: DataFrame containing the loaded data.
+    """
+
+    try:
+        data = pd.read_csv(file_path)
+        return data
+    except Exception as e:
+        print(f"Error loading data: {e}")
+        return None
+    
+
+def unique_smiles_count(df):
+    """
+    Count unique SMILES strings in the DataFrame.
+    
+    Parameters:
+    df (pd.DataFrame): DataFrame containing the data.
+    
+    Returns:
+    int: Number of unique SMILES strings.
+    """
+    
+    if 'SMILES' in df.columns:
+        return df['SMILES'].nunique()
+    else:
+        print("Column 'SMILES' not found in DataFrame.")
+        return 0
+
+
+def non_unique_smiles_count(df):
+    """
+    Count non-unique SMILES strings in the DataFrame.
+    
+    Parameters:
+    df (pd.DataFrame): DataFrame containing the data.
+    
+    Returns:
+    int: Number of non-unique SMILES strings.
+    """
+    
+    if 'SMILES' in df.columns:
+        return df['SMILES'].shape[0] - df['SMILES'].nunique()
+    else:
+        print("Column 'SMILES' not found in DataFrame.")
+        return 0
+
+
+
+
+
+
+if __name__ == "__main__":
+
+    # Load the MOSES dataset
+    print("Loading MOSES dataset...")
+    moses = load_data(moses_data_path)
+    moses_train = load_data(moses_data_train_path)
+    moses_test = load_data(moses_data_test_path)
+
+    print("HEAD MOSES DATASET\n"+moses.head())
+    print("MOSES nujmber train test\n")
+    print(moses["SPLIT"].value_counts())
+    print("Unique SMILES in MOSES dataset: ", unique_smiles_count(moses))
+    print("Non unique SMILES in MOSES dataset: ", non_unique_smiles_count(moses))
+
+    print("Unique SMILES in MOSES train: ", unique_smiles_count(moses_train))
+    print("Non unique SMILES in MOSES train: ", non_unique_smiles_count(moses_train))
+
+    print("Unique SMILES in MOSES dataset: ", unique_smiles_count(moses_test))
+    print("Non unique SMILES in MOSES dataset: ", non_unique_smiles_count(moses_test))
+
+    print("moses SMILES object types: ",moses['SMILES'].apply(type).value_counts())
+    print("Type moses: ", moses.dtypes)
+    
+    print(moses["SMILES"][0])
